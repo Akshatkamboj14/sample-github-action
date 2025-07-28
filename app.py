bf8ca86012a5485be46b98ae03002b20cd3859ae
Ubuntu <ubuntu@ip-172-31-43-85.ec2.internal>
2025-07-28 08:41:51 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return f"Hello from Flask! Running on {os.uname().nodename}\n"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
